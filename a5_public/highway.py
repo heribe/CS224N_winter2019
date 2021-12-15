d9b59c87e80851c4a19bd834c2385f5da816be4a
zhbmacbookpro <zhb384708358@163.com>
2021-12-15 10:35:14 +0800

    complete 1h

--- a5_public/highway.py
+++ a5_public/highway.py
@@ -6,7 +6,31 @@ CS224N 2018-19: Homework 5
 """
 
 ### YOUR CODE HERE for part 1h
+import torch.nn as nn
+import torch
 
+# Make sure that your module uses two nn.Linear layers (this is important for the autograder)
+class Highway(nn.Module):
+    #def __init__(self,embed_word,prob):
+    def __init__(self,embed_word,prob):
+        super(Highway,self).__init__()
+        self.linear1 = nn.Linear(embed_word,embed_word,bias=True)
+        self.linear2 = nn.Linear(embed_word,embed_word,bias=True)
+        self.relu = nn.ReLU()
+        self.sig = nn.Sigmoid()
+        #self.drop = nn.Dropout(prob)
+
+    def forward(self,x_conv_out):
+        """
+        @param x_conv_out (Tensor): shape of input tensor [max_sentence_length, batch_size, max_word_length,1,e_word]
+        @return x_emb(Tensor): shape [max_sentence_length, batch_size, max_word_length,e_word]
+        """
+        x_proj = self.relu(self.linear1(x_conv_out)) 
+        x_gate = self.sig(self.linear2(x_conv_out))
+        x_hightway = torch.mul(x_gate,x_proj) + torch.mul((1-x_gate),x_conv_out)
+       # x_emb = self.drop(x_hightway)
+       # return x_emb
+        return x_hightway
 
 ### END YOUR CODE 
 
